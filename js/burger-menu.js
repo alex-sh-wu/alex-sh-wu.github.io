9ca33a6e828d37adc03a7ab1a4c614a2667dceb2
alex-sh-wu <alexex00001@hotmail.com>
2019-01-13 00:12:31 -0500

    make burger menu a little less hardcoded using javascript

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -7,6 +7,14 @@ window.addEventListener("load", function() {
 
   //alert("This website is optimized for Google Chrome only. Please switch your browser for a better viewing experience.");
 
+  var burger_menu_items = document.getElementsByClassName("option");
+  for (var i = 0; i < burger_menu_items.length; i++) {
+    if (burger_menu_items[i].children[0].href === window.location.href) {
+      burger_menu_items[i].classList.add("active");
+      burger_menu_items[i].children[0].href = "#"
+    }
+  };
+
   burger_menu_logo.addEventListener("click", function() {
     if (burger_menu.style.left === "-150px") {
       burger_menu.style.left = "0px";
